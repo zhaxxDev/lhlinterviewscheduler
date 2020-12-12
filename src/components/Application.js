8e668421a56389a5877c4eb5c1461b8bba162df7
zhaxxdev <alex.xian@Hotmail.com>
2020-12-11 17:14:19 -0800

    fixed some bugs while it still works

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,77 +6,14 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 
-const appointments = [
-	{
-		id: 1,
-		time: "12pm",
-		interview: {
-			student: "Lewis Hamilton",
-			interviewer: {
-				id: 1,
-				name: "Sylvia Palmer",
-				avatar: "https://i.imgur.com/LpaY82x.png",
-			},
-		},
-	},
-	{
-		id: 2,
-		time: "1pm",
-		interview: {
-			student: "Kimi Raikonnen",
-			interviewer: {
-				id: 1,
-				name: "Sylvia Palmer",
-				avatar: "https://i.imgur.com/LpaY82x.png",
-			},
-		},
-	},
-	{
-		id: 3,
-		time: "2pm",
-		interview: {
-			student: "Sebatian Vettel",
-			interviewer: {
-				id: 1,
-				name: "Sylvia Palmer",
-				avatar: "https://i.imgur.com/LpaY82x.png",
-			},
-		},
-	},
-	{
-		id: 4,
-		time: "3pm",
-		interview: {
-			student: "George Russel",
-			interviewer: {
-				id: 1,
-				name: "Sylvia Palmer",
-				avatar: "https://i.imgur.com/LpaY82x.png",
-			},
-		},
-	},
-	{
-		id: 5,
-		time: "4pm",
-		interview: {
-			student: "Max Vershtappen",
-			interviewer: {
-				id: 1,
-				name: "Sylvia Palmer",
-				avatar: "https://i.imgur.com/LpaY82x.png",
-			},
-		},
-	},
-];
-
 export default function Application(props) {
 	const [state, setState] = useState({
 		day: "Monday",
 		days: [],
-		// you may put the line below, but will have to remove/comment hardcoded appointments variable
 		appointments: {},
 	});
-
+	const setDay = (day) => setState({ ...state, day });
+	const dailyAppointments = [];
 	const setDays = (days) => {
 		setState((prev) => ({ ...prev, days }));
 	};
@@ -93,7 +30,7 @@ export default function Application(props) {
 				/>
 				<hr className="sidebar__separator sidebar--centered" />
 				<nav className="sidebar__menu">
-					<DayList days={state.days} day={state.day} /* setDay={state.day}*/ />
+					<DayList days={state.days} day={state.day} setDay={setDay} />
 				</nav>
 				<img
 					className="sidebar__lhl sidebar--centered"
@@ -102,7 +39,7 @@ export default function Application(props) {
 				/>
 			</section>
 			<section className="schedule">
-				{appointments.map((appointment) => (
+				{dailyAppointments.map((appointment) => (
 					<Appointment key={appointment.id} {...appointment} />
 				))}
 				<Appointment key="last" time="5pm" />
