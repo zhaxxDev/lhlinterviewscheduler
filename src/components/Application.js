c2d56f018b24822e954414d1bb069c2fd96000d9
zhaxxdev <alex.xian@Hotmail.com>
2020-12-11 17:05:31 -0800

    added state usage for the days

--- src/components/Application.js
+++ src/components/Application.js
@@ -70,16 +70,19 @@ const appointments = [
 ];
 
 export default function Application(props) {
-	const [day, setDay] = useState("Monday");
-	const [days, setDays] = useState([]);
+	const [state, setState] = useState({
+		day: "Monday",
+		days: [],
+		// you may put the line below, but will have to remove/comment hardcoded appointments variable
+		appointments: {},
+	});
+
+	const setDays = (days) => {
+		setState((prev) => ({ ...prev, days }));
+	};
 	useEffect(() => {
-		console.log("Run Useeffect ");
-		axios.get("/api/days").then((response) => {
-			console.log(response.data);
-			setDays(response.data);
-		});
+		axios.get("/api/days").then((response) => setDays(response.data));
 	}, []);
-	console.log(days);
 	return (
 		<main className="layout">
 			<section className="sidebar">
@@ -90,7 +93,7 @@ export default function Application(props) {
 				/>
 				<hr className="sidebar__separator sidebar--centered" />
 				<nav className="sidebar__menu">
-					<DayList days={days} day={day} setDay={setDay} />
+					<DayList days={state.days} day={state.day} /* setDay={state.day}*/ />
 				</nav>
 				<img
 					className="sidebar__lhl sidebar--centered"
