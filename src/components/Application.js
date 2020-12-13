6c6b6f45791432f0af47541472e0efe5a6964d8d
zhaxxdev <alex.xian@Hotmail.com>
2020-12-12 18:32:05 -0800

    added interviews in state

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,15 +6,31 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 import { getAppointmentsForDay } from "helpers/selectors";
+import { getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 	const [state, setState] = useState({
 		day: "Monday",
 		days: [],
 		appointments: {},
+		interviewers: {},
 	});
 	const setDay = (day) => setState({ ...state, day });
 	const dailyAppointments = getAppointmentsForDay(state, state.day);
+	const appointments = getAppointmentsForDay(state, state.day);
+
+	const schedule = appointments.map((appointment) => {
+		const interview = getInterview(state, appointment.interview);
+
+		return (
+			<Appointment
+				key={appointment.id}
+				id={appointment.id}
+				time={appointment.time}
+				interview={interview}
+			/>
+		);
+	});
 	useEffect(() => {
 		Promise.all([
 			axios.get("http://localhost:8001/api/days"),
@@ -27,10 +43,10 @@ export default function Application(props) {
 				appointments: all[1].data,
 				interviewers: all[2].data,
 			}));
-			console.log(all[1].data);
+			console.log(all[2].data);
 		});
 	}, []);
-	console.log(dailyAppointments);
+	console.log(state.interviewers);
 	return (
 		<main className="layout">
 			<section className="sidebar">
@@ -50,9 +66,7 @@ export default function Application(props) {
 				/>
 			</section>
 			<section className="schedule">
-				{dailyAppointments.map((appointment) => (
-					<Appointment key={appointment.id} {...appointment} />
-				))}
+				{dailyAppointments.map((appointment) => schedule)}
 				<Appointment key="last" time="5pm" />
 			</section>
 		</main>
